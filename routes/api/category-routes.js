03dcf6d7eb31254cff5045a20447c4eb0ec81742
Morgan Sherrill <mo.sherrill@outlook.com>
2021-12-15 00:53:05 -0800

    finished delete and post routes in category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,16 +38,42 @@ router.get('/:id', async (req, res) => {
   }
 })
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+  const categoryData = await Category.findByPk(req.params.id)
+  categoryData.category_name = req.body.category_name
+
+  res.status(200).json(categoryData)
+
+} catch (err) {
+  console.log(err)
+  res.status(500).json(err);
+}
+})
+
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const CategoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
